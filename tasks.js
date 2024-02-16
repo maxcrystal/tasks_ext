c105913f9c6bb7e0086028c151ee28b1832ce667
maxcrystal <hmi@ya.ru>
2024-02-16 12:38:43 +0300

    Initial commit

---
+++ tasks.js
@@ -0,0 +1,13 @@
+
+const taskInput = document.getElementById('idInput');
+
+// Set taskInput in focus
+taskInput.focus();
+
+// Add event listener to input field for Enter key press
+taskInput.addEventListener("keyup", function(event) {
+    // Check if the Enter key is pressed (key code 13)
+    if (event.keyCode === 13) {
+        window.open('https://portal.tokeon.ru/task/'+taskInput.value, '_blank');
+    }
+});
